5c8f6f06b86dcfc6d2f58bcf2a6b1d0ae054e7df
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2025-08-27 17:11:08 -0700

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,68 @@
+##############################################################################################################
+
+#CASE SENSITIVE
+
+#variables
+item = 'appple'
+Item = 'orange'
+item_fruit = 'grape'
+
+print(item, Item, item_fruit)
+
+#data types
+integer = 2025
+string = 'words'
+boolean = True | False
+lists = ['mario, 'luigi', 111]
+
+
+#math
+a = 10
+b = 4
+
+addition = a + b
+difference = a - b
+multiply = a * b
+divide = a/b
+power = a**b
+
+
+#basic logic statement
+age = 28
+isHappy = FALSE
+
+if age > 21:
+  print('string')
+        elif age == 18:
+        print('string')
+else:
+   print('string')
+
+
+#Loops
+for i in range(3)
+print('')
+
+#break = cancelled loop while iterating
+
+
+#functions
+def function();
+  pass
+# insert desired function
+
+#pass is for unmapped functions, it's an error handling tool and lets the code run still even though the function isn't 'assigned'
+#example
+
+
+#try and except:
+number = input('please provide number >>')
+
+try:
+  print(10 + int(number))
+
+except:
+  print('that is not a number mate')
+
+
+######################################################################################################################
